ba6b69373cf281b093df9301e4943d2a61863086
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-07-27 20:36:42 -0500

    TLR: Adding copyright notices to all files.

--- trafficlight_recognizer/nodes/feat_proj/feat_proj.cpp
+++ trafficlight_recognizer/nodes/feat_proj/feat_proj.cpp
@@ -1,29 +1,41 @@
 /*
- * signals.cpp
- *
- *  Created on: Apr 9, 2015
- *      Author: sujiwo
+ * Copyright 2015 sujiwo
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#include <iostream>
-#include <ros/ros.h>
 #include "trafficlight_recognizer/rate.h"
 #include "libvectormap/vector_map.h"
-#include <tf/tf.h>
-#include <tf/transform_listener.h>
-#include <sensor_msgs/CameraInfo.h>
-#include <geometry_msgs/TwistStamped.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseStamped.h>
+#include "libvectormap/Math.h"
+
+#include <iostream>
 #include <signal.h>
 #include <cstdio>
-#include "libvectormap/Math.h"
+
 #include <Eigen/Eigen>
-#include <autoware_msgs/Signals.h>
+
+#include <ros/ros.h>
 #include <autoware_msgs/AdjustXY.h>
+#include <autoware_msgs/Lane.h>
+#include <autoware_msgs/Signals.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
 #include <vector_map/vector_map.h>
 #include <vector_map_server/GetSignal.h>
-#include <autoware_msgs/Lane.h>
 
 static std::string camera_id_str;
 
--- trafficlight_recognizer/nodes/region_tlr/region_tlr.cpp
+++ trafficlight_recognizer/nodes/region_tlr/region_tlr.cpp
@@ -1,25 +1,42 @@
+/*
+ * Copyright 2019 Autoware Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "trafficlight_recognizer/traffic_light.h"
+#include "trafficlight_recognizer/region_tlr/region_tlr.h"
+
 #include <vector>
 #include <float.h>
 #include <math.h>
 #include <sstream>
+
 #include <ros/ros.h>
+#include <autoware_msgs/Signals.h>
+#include <autoware_msgs/TrafficLight.h>
+#include <autoware_msgs/TrafficLightResult.h>
+#include <autoware_msgs/TrafficLightResultArray.h>
+#include <autoware_msgs/TunedResult.h>
 #include <cv_bridge/cv_bridge.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Bool.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <autoware_msgs/TrafficLight.h>
-#include <autoware_msgs/Signals.h>
-#include <autoware_msgs/TunedResult.h>
-#include <autoware_msgs/TrafficLightResultArray.h>
-#include <autoware_msgs/TrafficLightResult.h>
-
-#include "trafficlight_recognizer/traffic_light.h"
-#include "trafficlight_recognizer/region_tlr/region_tlr.h"
 
 thresholdSet thSet;
 
