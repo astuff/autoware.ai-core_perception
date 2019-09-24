64cf9dcca328d0ca3943dc5df5bc9255721664ea
Ian Colwell <icolwell@autonomoustuff.com>
2019-09-24 10:28:15 -0400

    Add rgb8 encoding support

--- trafficlight_recognizer/nodes/region_tlr_tensorflow/region_tlr_tensorflow.cpp
+++ trafficlight_recognizer/nodes/region_tlr_tensorflow/region_tlr_tensorflow.cpp
@@ -1,12 +1,12 @@
 /*
  * Copyright 2019 AutonomouStuff, LLC
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -50,7 +50,11 @@ void RegionTLRTensorFlowROSNode::ImageRawCallback(const sensor_msgs::ImageConstP
 {
   auto se_frame = cv_bridge::toCvShare(msg);
 
-  if (msg->encoding == "bgr8")
+  if (msg->encoding == "rgb8")
+  {
+    frame_ = se_frame->image;
+  }
+  else if (msg->encoding == "bgr8")
   {
     cv::cvtColor(se_frame->image, frame_, cv::COLOR_BGR2RGB);
   }
